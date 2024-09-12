f186c9c0d19c694d092d47df7dd0f20da05f510b
Sazid <mahmuduzzamansazid@gmail.com>
2024-09-12 17:34:57 +0600

    new commit

--- src/components/LearningCards.jsx
+++ src/components/LearningCards.jsx
@@ -117,20 +117,40 @@ function LearningCards() {
             <h5 className="card-title">Run First Flutter App</h5> <hr />
             <ul>
               <li>
-                <p className="card-text">Open <u><b>Android Studio</b></u></p>
-              </li>
-              <li>
-                <p className="card-text">Open <u><b>appname file</b></u></p>
-              </li>
-              <li>
-                <p className="card-text">Connect <u><b>Emulator</b></u>
-                <ul><li>Virtual Emulator</li>
-                <li>Physical Device</li>
-                </ul>
+                <p className="card-text">
+                  Open{" "}
+                  <u>
+                    <b>Android Studio</b>
+                  </u>
                 </p>
               </li>
               <li>
-                <p className="card-text"><u><b>Run</b></u> Flutter Demo App</p>
+                <p className="card-text">
+                  Open{" "}
+                  <u>
+                    <b>appname file</b>
+                  </u>
+                </p>
+              </li>
+              <li>
+                <p className="card-text">
+                  Connect{" "}
+                  <u>
+                    <b>Emulator</b>
+                  </u>
+                  <ul>
+                    <li>Virtual Emulator</li>
+                    <li>Physical Device</li>
+                  </ul>
+                </p>
+              </li>
+              <li>
+                <p className="card-text">
+                  <u>
+                    <b>Run</b>
+                  </u>{" "}
+                  Flutter Demo App
+                </p>
               </li>
             </ul>
           </div>
@@ -146,20 +166,60 @@ function LearningCards() {
             <h5 className="card-title">Create Hello World App</h5> <hr />
             <h5>Step-1: Delete Demo Code</h5>
             <ul>
-              <li>Open <u><b>Project Files</b></u></li>
-              <li>Open <u><b>lib</b></u> folder</li>
-              <li>Open <u><b>main.dart</b></u> files</li>
+              <li>
+                Open{" "}
+                <u>
+                  <b>Project Files</b>
+                </u>
+              </li>
+              <li>
+                Open{" "}
+                <u>
+                  <b>lib</b>
+                </u>{" "}
+                folder
+              </li>
+              <li>
+                Open{" "}
+                <u>
+                  <b>main.dart</b>
+                </u>{" "}
+                files
+              </li>
               <li>Delete all code</li>
             </ul>
           </div>
           <div className="card-body">
             <h5>Step-2: Write New Code</h5>
-            <p>There are <b>4 steps</b> to Run a new Flutter App</p>
+            <p>
+              There are <b>4 steps</b> to Run a new Flutter App
+            </p>
             <ul>
-              <li>import <u><b>Material.dart</b></u> file</li>
-              <li>create <u><b>Main function</b></u></li>
-              <li>create an <u><b>app widget</b></u></li>
-              <li>create a <u><b>screen widget</b></u></li>
+              <li>
+                import{" "}
+                <u>
+                  <b>Material.dart</b>
+                </u>{" "}
+                file
+              </li>
+              <li>
+                create{" "}
+                <u>
+                  <b>Main function</b>
+                </u>
+              </li>
+              <li>
+                create an{" "}
+                <u>
+                  <b>app widget</b>
+                </u>
+              </li>
+              <li>
+                create a{" "}
+                <u>
+                  <b>screen widget</b>
+                </u>
+              </li>
             </ul>
           </div>
           <img
@@ -172,48 +232,244 @@ function LearningCards() {
         <div className="card bg-dark mt-5">
           <div className="card-body">
             <h5 className="card-title">AppBar</h5> <hr />
-            <p>AppBar is a Material Design component that provides a consistent app bar across all screen sizes.</p>
+            <p>
+              AppBar is a Material Design component that provides a consistent
+              app bar across all screen sizes.
+            </p>
             <ul>
-              <li>import <u><b>AppBar.dart</b></u> file</li>
-              <li>create <u><b>AppBar</b></u> widget</li>
-              <li>add <u><b>AppBar</b></u> to your <u><b>build method</b></u></li>
+              <li>
+                import{" "}
+                <u>
+                  <b>AppBar.dart</b>
+                </u>{" "}
+                file
+              </li>
+              <li>
+                create{" "}
+                <u>
+                  <b>AppBar</b>
+                </u>{" "}
+                widget
+              </li>
+              <li>
+                add{" "}
+                <u>
+                  <b>AppBar</b>
+                </u>{" "}
+                to your{" "}
+                <u>
+                  <b>build method</b>
+                </u>
+              </li>
             </ul>
+
             
             <h5>Proparties:</h5>
 
-            <ul>
-              <li>backgroundColor: <u><b>Color</b></u> (optional)</li> 
-              <li>toolbarHeight: <u><b>double</b></u> (optional)</li>
-              {<br/>}
-              <li>title: <u><b>Text</b></u> widget</li>
-              <li>titleTextStyle: <u><b>TextStyle</b></u> (optional)</li>
-              <li>titleSpacing: <u><b>double</b></u> (optional)</li>
-              <li>centerTitle: <u><b>bool</b></u> (optional)</li>
-              <li>showTitle: <u><b>bool</b></u> (optional)</li>
-              {<br/>}
-              <li>actions: <u><b>List</b></u> of <u><b>IconButton</b></u> widgets</li>
-              <li>actions: <u><b>List</b></u> of <u><b>Widget</b></u> widgets</li>
-              <li>leading: <u><b>Widget</b></u> (optional)</li>
-              <li>elevation: <u><b>double</b></u> (optional)</li>
-              <li>shape: <u><b>value</b></u> (optional)</li>
-              <li>primary: <u><b>bool</b></u> (optional)</li>
 
-              <li>fixed: <u><b>bool</b></u> (optional)</li>
-              <li>extendBody: <u><b>bool</b></u> (optional)</li>
-              <li>scrollBehavior: <u><b>ScrollBehavior</b></u> (optional)</li>
-              <li>actionsIconSize: <u><b>double</b></u> (optional)</li>
-              <li>actionsIconTheme: <u><b>IconThemeData</b></u> (optional)</li>
-              <li>actionsLabelStyle: <u><b>TextStyle</b></u> (optional)</li>
-              <li>actionsIconSpacing: <u><b>double</b></u> (optional)</li>
-              <li>actionsMaxIconSize: <u><b>double</b></u> (optional)</li>
-              <li>actionsOverflowButton: <u><b>Widget</b></u> (optional)</li>
-              <li>actionsPosition: <u><b>AppBarActionsPosition</b></u> (optional)</li>
-              <li>actionsSpacing: <u><b>double</b></u> (optional)</li>
-              <li>actionsRowHeight: <u><b>double</b></u> (optional)</li>
-              <li>actionsTextTheme: <u><b>TextThemeData</b></u> (optional)</li>
-              <li>actionsWrap: <u><b>bool</b></u> (optional)</li>
-              <li>actionsAlignment: <u><b>Alignment</b></u> (optional)</li>
-              <li>actionsTextTheme: <u><b>TextThemeData</b></u> (optional)</li>
+            <ul>
+              <li>
+                backgroundColor:{" "}
+                <u>
+                  <b>Color</b>
+                </u>{" "}
+                (for AppBar background Color)
+              </li>
+              <li>
+                foregroundColor:{" "}
+                <u>
+                  <b>Color</b>
+                </u>{" "}
+                (for Icon Color)
+              </li>
+              <li>
+                shadowColor:{" "}
+                <u>
+                  <b>Color</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                surfaceTintColor:{" "}
+                <u>
+                  <b>Color</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                toolbarHeight:{" "}
+                <u>
+                  <b>double</b>
+                </u>{" "}
+                (for customize AppBar Height)
+              </li>
+              <li>
+                toolbarTextStyle:{" "}
+                <u>
+                  <b>TextStyle</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                toolbarOpacity:{" "}
+                <u>
+                  <b>bool</b>
+                </u>{" "}
+                (1.0)
+              </li>
+              <li>
+                bottomOpacity:{" "}
+                <u>
+                  <b>bool</b>
+                </u>{" "}
+                (1.0)
+              </li>
+              <li>
+                shape:{" "}
+                <u>
+                  <b>ShapeBorder</b>
+                </u>{" "}
+                (optional)
+              </li>
+              {<br />}
+              <li>
+                leading:{" "}
+                <u>
+                  <b>Widget</b>
+                </u>{" "}
+                (for left icon)
+              </li>
+              <li>
+                leadingWidth:{" "}
+                <u>
+                  <b>double</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                automaticallyImplyLeading:{" "}
+                <u>
+                  <b>bool</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                title:{" "}
+                <u>
+                  <b>Text</b>
+                </u>{" "}
+                widget
+              </li>
+              <li>
+                titleTextStyle:{" "}
+                <u>
+                  <b>TextStyle</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                titleSpacing:{" "}
+                <u>
+                  <b>double</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                centerTitle:{" "}
+                <u>
+                  <b>bool</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                actions:{" "}
+                <u>
+                  <b>List</b>
+                </u>{" "}
+                of{" "}
+                <u>
+                  <b>IconButton</b>
+                </u>{" "}
+                widgets (for right icon)
+              </li>
+              {<br />}
+              <li>
+                flexibleSpace:{" "}
+                <u>
+                  <b>widget</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                bottom:{" "}
+                <u>
+                  <b>PreferredSizeWidget</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                elevation:{" "}
+                <u>
+                  <b>double</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                scrolledUnderElevation:{" "}
+                <u>
+                  <b>double</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                iconTheme:{" "}
+                <u>
+                  <b>IconThemeData</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                actionsIconTheme:{" "}
+                <u>
+                  <b>IconThemeData</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                primary:{" "}
+                <u>
+                  <b>bool</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                excludeHeaderSemantics:{" "}
+                <u>
+                  <b>bool</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                systemOverlayStyle:{" "}
+                <u>
+                  <b>systemUiOverlayStyle</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                forceMaterialTransparency:{" "}
+                <u>
+                  <b>bool</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                clipBehavior:{" "}
+                <u>
+                  <b>Clip</b>
+                </u>{" "}
+                (optional)
+              </li>
             </ul>
           </div>
           <img
@@ -222,31 +478,110 @@ function LearningCards() {
             alt="Download & Install Flutter"
           />
         </div>
-        {/* BottomAppBar ==============================*/}
+        {/* BottomNavigationBar ==============================*/}
         <div className="card bg-dark mt-5">
           <div className="card-body">
-            <h5 className="card-title">BottomAppBar</h5> <hr />
+            <h5 className="card-title">BottomNavigationBar</h5> <hr />
             <p>
-              BottomAppBar is a Material Design component that provides a consistent app bar at the bottom of the screen. It allows you to add actions, icons, and text to your app.
+              BottomAppBar is a Material Design component that provides a
+              consistent app bar at the bottom of the screen. It allows you to
+              add actions, icons, and text to your app.
             </p>
             <ul>
-              <li>import <u><b>BottomAppBar.dart</b></u> file</li>
-              <li>create <u><b>BottomAppBar</b></u> widget</li>
-              <li>add <u><b>BottomAppBar</b></u> to your <u><b>build method</b></u></li>
+              <li>
+                import{" "}
+                <u>
+                  <b>BottomAppBar.dart</b>
+                </u>{" "}
+                file
+              </li>
+              <li>
+                create{" "}
+                <u>
+                  <b>BottomAppBar</b>
+                </u>{" "}
+                widget
+              </li>
+              <li>
+                add{" "}
+                <u>
+                  <b>BottomAppBar</b>
+                </u>{" "}
+                to your{" "}
+                <u>
+                  <b>build method</b>
+                </u>
+              </li>
             </ul>
-            
             <h5>Proparties:</h5>
-
             <ul>
-              <li>backgroundColor: <u><b>Color</b></u> (optional)</li>
-              <li>elevation: <u><b>double</b></u> (optional)</li>
-              <li>toolbarHeight: <u><b>double</b></u> (optional)</li>
-              <li>shape: <u><b>value</b></u> (optional)</li>
-              <li>contentPadding: <u><b>EdgeInsets</b></u> (optional)</li>
-              <li>actions: <u><b>List</b></u> of <u><b>Widget</b></u> widgets</li>
-              <li>actionsIconSize: <u><b>double</b></u> (optional)</li>
-              <li>actionsIconTheme: <u><b>IconThemeData</b></u> (optional)</li>
-              <li>actionsTextTheme: <u><b>TextThemeData</b></u> (optional)</li>
+              <li>
+                backgroundColor:{" "}
+                <u>
+                  <b>Color</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                elevation:{" "}
+                <u>
+                  <b>double</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                toolbarHeight:{" "}
+                <u>
+                  <b>double</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                shape:{" "}
+                <u>
+                  <b>value</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                contentPadding:{" "}
+                <u>
+                  <b>EdgeInsets</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                actions:{" "}
+                <u>
+                  <b>List</b>
+                </u>{" "}
+                of{" "}
+                <u>
+                  <b>Widget</b>
+                </u>{" "}
+                widgets
+              </li>
+              <li>
+                actionsIconSize:{" "}
+                <u>
+                  <b>double</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                actionsIconTheme:{" "}
+                <u>
+                  <b>IconThemeData</b>
+                </u>{" "}
+                (optional)
+              </li>
+              <li>
+                actionsTextTheme:{" "}
+                <u>
+                  <b>TextThemeData</b>
+                </u>{" "}
+                (optional)
+              </li>
             </ul>
           </div>
           <img
